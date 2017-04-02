78e520ca97e78e2fc90d82a2fa8080a0ef300888
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-04-02 12:06:18 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+require('source-map-support').install();
+
+export * from "FileUtils";
+export * from "Generator";
+export * from "GeneratorBase";
+export * from "GeneratorEbook";
+export * from "GeneratorHTML";
+export * from "GeneratorPDF";
+export * from "MDUtils";
